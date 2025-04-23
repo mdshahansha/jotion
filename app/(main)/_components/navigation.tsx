ce523e5c0be86057bc7814106a0dbafe1157a27e
mdshahansha <mdshahansha44411@gmail.com>
2025-04-23 18:39:17 +0530

    added skeletoon ,navbar ,edit  notes name ,...

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-items";
@@ -13,12 +13,14 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { TrashBox } from "./trash-box";
 import { useSearch } from "@/hooks/user-search";
 import { useSettings } from "@/hooks/use-settings";
+import { Navbar } from "./navbar";
 
 const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width:768px)");
-    const search=useSearch();
-    const settings=useSettings();
+    const search = useSearch();
+    const settings = useSettings();
+    const params = useParams();
 
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -177,9 +179,24 @@ const Navigation = () => {
                 isResetting && "transition-all ease-in-out duration-300",
                 isMobile && "left-0 w-full"
             )}>
-                <nav className="bg-transparent px-3 py-2 w-full">
-                    {isCollapsed && <MenuIcon className="h-6 w-6 text-muted-foreground" />}
-                </nav>
+
+                {
+                    !!params.documentId ? (
+                        <Navbar
+                            isCollapsed={isCollapsed}
+                            onResetWidth={resetWidth}
+                        />
+
+                    ) : (
+                        <nav className="bg-transparent px-3 py-2 w-full">
+                            {isCollapsed && <MenuIcon className="h-6 w-6 text-muted-foreground" />}
+                        </nav>
+                    )
+                }
+
+
+
+
             </div>
 
         </>
